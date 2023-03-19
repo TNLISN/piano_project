ba9b5e05733497692fd30035e935eda4230a7c6a
TNLISN <nicolas45lav@gmail.com>
2023-03-19 12:05:21 +0100

    Add piano react and sound

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import Soundfont from "soundfont-player";
+import "react-piano/dist/styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const firstNote = MidiNumbers.fromNote("c3");
+  const lastNote = MidiNumbers.fromNote("f5");
+  const keyboardShortcuts = KeyboardShortcuts.create({
+    firstNote: firstNote,
+    lastNote: lastNote,
+    keyboardConfig: KeyboardShortcuts.HOME_ROW,
+  });
 
-export default App;
+  const [audioContext, setAudioContext] = useState(null);
+  const [instrument, setInstrument] = useState(null);
+
+  useEffect(() => {
+    const newAudioContext = new (window.AudioContext || window.webkitAudioContext)();
+    setAudioContext(newAudioContext);
+    Soundfont.instrument(newAudioContext, "acoustic_grand_piano").then((piano) => {
+      setInstrument(piano);
+    });
+
+    return () => {
+      if (newAudioContext) {
+        newAudioContext.close();
+      }
+    };
+  }, []);
+  
+  return (
+    <Piano
+      noteRange={{ first: firstNote, last: lastNote }}
+      playNote={(midiNumber) => {
+        if (instrument) {
+          instrument.play(midiNumber);
+        }
+      }}
+      stopNote={(midiNumber) => {
+          if (instrument) {
+            instrument.stop(midiNumber);
+          }
+      }}
+        width={700}
+        keyboardShortcuts={keyboardShortcuts}
+      />
+    );
+  }
+
+  export default App;
